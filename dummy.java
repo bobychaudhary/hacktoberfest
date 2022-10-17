0e7dc52668ac2cd7cc74832af86d044f6fd8ca27
mangalam1234 <115993850+mangalam1234@users.noreply.github.com>
2022-10-17 17:12:17 +0530

    Update dummy.java
    use this optimized way to solve this

--- dummy.java
+++ dummy.java
@@ -1,47 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-public class A_Array_Rearrangment {
-    public static void main(String[] args ){
-        Scanner sc=new Scanner(System.in);
-        int t= sc.nextInt();
+public class A_Gamer_Hemose {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
         while(t-->0){
             int n=sc.nextInt();
-            int x= sc.nextInt();
-            int[] arr=new int[n];
-            int[] brr=new int[n];
-            for(int i=0;i<n;i++){
-                arr[i]=sc.nextInt();
-            }
-            for(int i=0;i<n;i++){
-                brr[i]=sc.nextInt();
-            }
-            Arrays.sort(arr);
-            Arrays.sort(brr);
-            int temp=0;
-            for (int i = 0; i < brr.length; i++) {
-                for (int j = i+1; j < brr.length; j++) {
-                    if(arr[i] < arr[j]) {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
-            }
-            int count=0;
+            int h= sc.nextInt();
+            ArrayList<Integer> arr=new ArrayList<>();
             for (int i = 0; i < n; i++) {
-                if (arr[i]+brr[i]<=x){
-                    count++;
+                arr.add(sc.nextInt());
+            }
+            int a=0;
+            int b=0;
+            for (int i = 0; i < n; i++) {
+                int input = arr.get(i);
+                if (input >= a) {
+                    b = a;
+                    a = input;
+                } else if (input > b) {
+                    b = input;
                 }
             }
-            if(count==n){
-                System.out.println("YES");
-            }
-            else{
-                System.out.println("NO");
-            }
+            int count = (h % (a + b)) == 0 ? 2 * (h / (a + b))
+                    : (h % (a + b)) <= a ? 2 * (h / (a + b)) + 1 : 2 * ( h/ (a + b)) + 2;
+
+
+        System.out.println(count);
+    }  }
         }
-    }
-}
+
